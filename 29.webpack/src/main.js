9dc51c69767962028994d6c2f404d3a5065d3f19
yuan <yuan.yuan486@chinaredstar.com>
2018-03-20 17:02:48 +0800

    commit

--- 29.webpack/src/main.js
+++ 29.webpack/src/main.js
@@ -1,5 +1,18 @@
+import color from './color'
+
 require('./index.css')
+require('./less.less')
+require('./sass.scss')
+require('bootstrap');
 let src = require('./images/avatar.png')
 let img = new Image()
 img.src = src
 document.body.appendChild(img)
+
+console.log(color)
+let getColor = ()=>{
+    return color
+}
+
+
+console.log(getColor())
--- 29.webpack/webpack.config.js
+++ 29.webpack/webpack.config.js
@@ -1,8 +1,18 @@
 const path = require('path');
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
-const webpack = require('webpack')
-const WebpackWithimgPlugin = require('webpack-withimg-plugin')
+const ExreactTextWebpackPlugin =  require('extract-text-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+//npm i extract-text-webpack-plugin@next -D
+let cssExtract = new ExreactTextWebpackPlugin({
+  filename:'css.css',
+  allChunks:true
+})
+let lessExtract = new ExreactTextWebpackPlugin('less.css')
+let sassExtract = new ExreactTextWebpackPlugin('sass.css')
+
+//提取css文件 extract-text-webpack-plugin
 
 let pages = ['index', 'base'];
 pages = pages.map(page => new HtmlWebpackPlugin({
@@ -23,12 +33,21 @@ module.exports = {
     path:path.join(__dirname,'dist'),
     filename:'[name].[hash:8].js'
   },
+  resolve: {
+    //引入模块的时候，可以不用扩展名
+    extensions: [".js", ".less", ".json"],
+    alias: {//别名
+      "bootstrap": "bootstrap/dist/css/bootstrap.css"
+    }
+  },
+  watch:false,
+  watchOptions:{
+    ignored:/node_modules/,
+    poll:1000,
+    aggregateTimeout: 500, //防止重复保存频繁重新编译,500毫米内重复保存不打包
+  },
   module:{
     rules:[
-      {
-        test:/\.css$/,
-        loader:['style-loader','css-loader']
-      },
       {
         test: require.resolve('jquery'),
         use:{
@@ -37,12 +56,45 @@ module.exports = {
         }
       },
       {
-        test: /\.(png|jpg|gif|svg|bmp)/,
-        use: 'file-loader'
+        test: /\.(png|jpg|gif|svg|bmp|eot|woff|woff2|ttf)/,
+        use: {
+          loader: 'url-loader',
+          options:{
+            limit: 5 * 1024,
+            outputPath: 'images/'//指定文件拷贝目录
+          }
+        }
       },
       {
         test:/\.(html|htm)/,
         loader:'html-withimg-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: cssExtract.extract({
+          use: [{ loader:'css-loader?minimize'},"postcss-loader"]
+        })//['style-loader', 'css-loader']
+      },
+      {
+        test:/\.less$/,
+        loader: lessExtract.extract({
+          use: ['css-loader?minimize', 'less-loader']
+        })//['style-loader', 'css-loader','less-loader']
+      },
+      {
+        test:/\.scss$/,
+        loader: sassExtract.extract({
+          use: ['css-loader?minimize', 'sass-loader']
+        })//['style-loader', 'css-loader','sass-loader']
+      },
+      {
+        test:/\.js/,
+        use:{
+          loader:'babel-loader',
+          options:{
+            presets: ["env", "stage-0", "react"]
+          }
+        }
       }
     ]
   },
@@ -61,8 +113,20 @@ module.exports = {
         removeAttributeQuotes: true
       }
     }),
-    new CleanWebpackPlugin([path.join(__dirname, 'dist')])
+    new CleanWebpackPlugin([path.join(__dirname, 'dist')]),
+    cssExtract,
+    lessExtract,
+    sassExtract,
+    new CopyWebpackPlugin([{
+      from: path.join(__dirname, 'public'),//静态资源目录源地址
+      to: path.join(__dirname,'dist','piblic') //目标地址，相对于output的path目录
+    }]),
   ],
+  // source-map 把映射文件生成到单独的文件，最完整最慢
+  // cheap - module - source - map 在一个单独的文件中产生一个不带列映射的Map
+  // eval - source - map 使用eval打包源文件模块, 在同一个文件中生成完整sourcemap
+  // cheap - module - eval - source - map sourcemap和打包后的JS同行显示，没有映射列
+  devtool:"source-map",
   devServer:{
     contentBase:'./dist',
     host:'localhost',
